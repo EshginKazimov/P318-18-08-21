3cb0d9b594c7e6079eba68e3af5e36aaef8bf662
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2021-08-18 22:50:48 +0400

    Add files via upload

---
+++ GenericType/GenericType/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace GenericType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Wrong way
+
+            //MyIntList intList = new MyIntList();
+            //intList.AddItem(0);
+            //intList.AddItem(1);
+            //intList.AddItem(2);
+            //intList.AddItem(3);
+            //intList.ShowItems();
+
+            //MyStringList stringList = new MyStringList();
+            //stringList.AddItem("Elnur");
+            //stringList.AddItem("Rail");
+            //stringList.AddItem("Vuqar");
+            //stringList.AddItem("Fagan");
+            //stringList.ShowItems();
+
+            //MyPersonList personList = new MyPersonList();
+            //personList.AddItem(new Person());
+            //personList.AddItem(new Person());
+            //personList.AddItem(new Person());
+            //personList.ShowItems();
+
+            #endregion
+
+            #region Generic
+
+            //MyList<int> intList = new MyList<int>();
+            //intList.AddItem(1);
+            //intList.AddItem(2);
+            //intList.AddItem(4);
+            //intList.AddItem(3);
+            //intList.ShowItems();
+
+            //MyList<string> stringList = new MyList<string>();
+            //stringList.AddItem("Elnur");
+            //stringList.AddItem("Rail");
+            //stringList.AddItem("Vuqar");
+            //stringList.ShowItems();
+
+            //MyList<Person> personList = new MyList<Person>();
+            //personList.AddItem(new Person());
+            //personList.AddItem(new Person());
+            //personList.AddItem(new Person());
+            //personList.ShowItems();
+
+            MyList<Person, int> myList = new MyList<Person, int>();
+
+            #endregion
+        }
+    }
+
+    #region Wrong way
+
+    class MyIntList
+    {
+        private int[] _numbers;
+
+        public MyIntList()
+        {
+            _numbers = new int[0];
+        }
+
+        public void AddItem(int number)
+        {
+            Array.Resize(ref _numbers, _numbers.Length + 1);
+            _numbers[_numbers.Length - 1] = number;
+        }
+
+        public void ShowItems()
+        {
+            foreach (int item in _numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class MyStringList
+    {
+        private string[] _numbers;
+
+        public MyStringList()
+        {
+            _numbers = new string[0];
+        }
+
+        public void AddItem(string number)
+        {
+            Array.Resize(ref _numbers, _numbers.Length + 1);
+            _numbers[_numbers.Length - 1] = number;
+        }
+
+        public void ShowItems()
+        {
+            foreach (string item in _numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class MyPersonList
+    {
+        private Person[] _numbers;
+
+        public MyPersonList()
+        {
+            _numbers = new Person[0];
+        }
+
+        public void AddItem(Person number)
+        {
+            Array.Resize(ref _numbers, _numbers.Length + 1);
+            _numbers[_numbers.Length - 1] = number;
+        }
+
+        public void ShowItems()
+        {
+            foreach (Person item in _numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Generic
+
+    //class MyList<T, U> where T:class
+    //class MyList<T, U> where T:class,new()
+    //class MyList<T, U> where T:struct
+    //class MyList<T, U> where T:class, IRun
+    //class MyList<T, U> where T:U
+    class MyList<T, U> where T:class where U:struct
+    {
+        //MyList class-i olsun, generic type-da
+        //private array field saxlasin
+        //ctor-da array uchun yer ayrilsin
+        //Add ve Remove methodlari olsun
+        private T[] _numbers;
+
+        public MyList()
+        {
+            _numbers = new T[0];
+        }
+
+        public void AddItem(T number)
+        {
+            Array.Resize(ref _numbers, _numbers.Length + 1);
+            _numbers[_numbers.Length - 1] = number;
+        }
+
+        public void ShowItems()
+        {
+            foreach (T item in _numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    #endregion
+
+    interface IRun
+    {
+
+    }
+
+    class Test
+    {
+
+    }
+
+    class Person : Test, IRun
+    {
+        public Person(string name)
+        {
+
+        }
+    }
+}
---
+++ GenericType/Homework/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+
+            //Console.WriteLine("Valyutani daxil edin");
+            //string input = Console.ReadLine();
+            //if (double.TryParse(input, out double result))
+            //{
+            //    Console.WriteLine(Converter(Currency.Dollar, result));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("yalnis input");
+            //}
+
+            #endregion
+
+            #region Task 2
+
+            //Console.WriteLine("Index-i daxil edin.");
+            //string input = Console.ReadLine();
+            //if (int.TryParse(input, out int result))
+            //{
+            //    Alphabet english = new Alphabet("English");
+            //    //english[0] = 'A';
+            //    //english[2] = 'C';
+            //    //Console.WriteLine(english[0]);
+            //    english.SetLetter(result, 'A');
+            //    //english.SetLetter(2, 'C');
+            //    Console.WriteLine(english.GetLetter(result));
+            //}
+
+            #endregion
+
+            #region Array sort
+
+            Person[] persons = ArraySort();
+            foreach (Person item in persons)
+            {
+                Console.WriteLine(item);
+            }
+            
+            #endregion
+        }
+
+        #region Task 1
+
+        static double Converter(Currency currency, double manat)
+        {
+            switch (currency)
+            {
+                case Currency.Dollar:
+
+                    return manat / 1.7;
+
+                case Currency.Euro:
+
+                    return manat / 1.98;
+
+                case Currency.Lira:
+
+                    return manat / 0.2;
+
+                case Currency.Yuan:
+
+                    return manat * 3.8;
+            }
+            return 0;
+        }
+
+
+        #endregion
+
+        #region Array sort
+
+        public static Person[] ArraySort()
+        {
+            Person p1 = new Person("Rail", "Jannatov", 24);
+            Person p2 = new Person("Sarkhan", "Efendizade", 23);
+            Person p3 = new Person("Vugar", "Rustamov", 39);
+
+            Person[] persons = { p1, p2, p3 };
+
+            Array.Sort(persons);
+            //for (int i = 0; i < persons.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j < persons.Length; j++)
+            //    {
+            //        if (persons[i] > persons[j])
+            //        {
+            //            Person temp = persons[i];
+            //            persons[i] = persons[j];
+            //            persons[j] = temp;
+            //        }
+            //    }
+            //}
+
+            return persons;
+        }
+
+        #endregion
+    }
+
+    #region Task 2
+
+    class Alphabet
+    {
+        public string Name { get; set; }
+        private char[] _letter;
+
+        public Alphabet(string name)
+        {
+            Name = name;
+            _letter = new char[30];
+
+        }
+        //public char this[int index]
+        //{
+        //    get
+        //    {
+        //        return _letter[index];
+        //    }
+        //    set
+        //    {
+        //        _letter[index] = value;
+        //    }
+        //}
+
+        public char GetLetter(int index)
+        {
+            return _letter[index];
+
+        }
+
+        public void SetLetter(int index, char symbol)
+        {
+            _letter[index] = symbol;
+
+        }
+
+    }
+
+
+    #endregion
+
+    #region Array sort
+
+    public class Person : /*IComparable,*/ IComparable<Person>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Age;
+        }
+
+        //public int CompareTo(object obj)
+        //{
+        //    Person p2 = (Person)obj;
+        //    return Age.CompareTo(p2.Age);
+        //}
+
+        public int CompareTo(Person p2)
+        {
+            return Age.CompareTo(p2.Age);
+        }
+    }
+
+    #endregion
+}
